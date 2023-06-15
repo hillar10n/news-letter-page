c8366e3c4d72d54a3ed8983af75dccf82bc2a058
hillarion <lansoyin@gmail.com>
2023-06-15 15:37:28 +0100

    javascript added

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-// const card = document.querySelector(".container");
-// const feedBack = document.querySelector(".thanks");
-// const form = document.querySelector("#subBtn");
-// const dismiss = document.querySelector("#dismiss");
+const card = document.querySelector(".container");
+const feedBack = document.querySelector(".thanks");
+const form = document.querySelector("#subBtn");
+const dismiss = document.querySelector("#dismiss");
 
-// form.addEventListener("submit", function (e) {
-// 	e.preventDefault();
-// 	card.style.display = "none";
-// 	feedBack.style.display = "block";
-// });
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+	card.style.display = "none";
+	feedBack.style.display = "block";
+});
 
-// dismiss.addEventListener("click", function () {
-// 	card.style.display = "";
-// 	feedBack.style.display = "none";
-// });
-
-const mPicture = document.querySelector("#mPicture");
-
-function checkPicture() {
-	if (window.innerWidth > 415) {
-		mPicture.style.display = "none";
-	}
-}
-checkPicture();
+dismiss.addEventListener("click", function () {
+	card.style.display = "";
+	feedBack.style.display = "none";
+});
