180027eafe26c53cef7881b665f842c09d5b6c7a
hillarion <lansoyin@gmail.com>
2023-06-16 12:30:16 +0100

    mobile completed

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const card = document.querySelector(".container");
 const feedBack = document.querySelector(".thanks");
 const form = document.querySelector("#subBtn");
 const dismiss = document.querySelector("#dismiss");
+const subEmail = document.querySelector("#subEmail");
+const input = document.querySelector("#email");
+const errorReport = document.querySelector("#invalidEmail");
 
 form.addEventListener("submit", function (e) {
 	e.preventDefault();
-	card.style.display = "none";
-	feedBack.style.display = "block";
+	validationCheck = input.value;
+	if (validationCheck.indexOf("@") === -1) {
+		errorReport.innerText = "Valid email required!";
+	} else {
+		card.classList.add("hidden");
+		feedBack.classList.remove("hidden");
+		subEmail.innerText = input.value;
+		input.value = "";
+	}
 });
 
 dismiss.addEventListener("click", function () {
-	card.style.display = "";
-	feedBack.style.display = "none";
+	card.classList.remove("hidden");
+	feedBack.classList.add("hidden");
 });
